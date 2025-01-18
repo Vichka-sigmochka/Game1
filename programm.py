26d26ca902bd2546603cf9f8535b785cd6ea7dd1
Вика <Vika1809dok@rambler.ru>
2025-01-18 18:02:36 +0300

    Vika commit 1

--- programm.py
+++ programm.py
@@ -1,3 +1,86 @@
-print('helo!')
+import os
+import sys
+import pygame
 
-print('Vika')
+
+class App:
+    def __init__(self):
+        pygame.init()
+        self.width, self.height = 700, 400
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption('GeometryDash')
+        self.fps = 50
+
+    def terminate(self):
+        pygame.quit()
+        sys.exit()
+
+    def load_image(self, name, colorkey=None):
+        fullname = os.path.join('data', name)
+        if not os.path.isfile(fullname):
+            print(f"Файл с изображением '{fullname}' не найден")
+            sys.exit()
+        image = pygame.image.load(fullname)
+        if colorkey is not None:
+            image = image.convert()
+            if colorkey == -1:
+                colorkey = image.get_at((0, 0))
+            image.set_colorkey(colorkey)
+        else:
+            image = image.convert_alpha()
+        return image
+
+    def run_game(self):
+        run = True
+        while run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.terminate()
+                if event.type == pygame.KEYDOWN: # переход на конечный фон спомощью нажатии клавиши.
+                    #Должен переходить только при заканчиваний жизни. Будут правки
+                    self.end_screen()
+                    run = False
+            self.screen.fill(pygame.Color('blue'))
+            pygame.display.flip()
+            self.clock.tick(self.fps)
+
+    def end_screen(self):
+        fon = pygame.transform.scale(self.load_image('end.jpg'), (self.width, self.height))
+        self.screen.blit(fon, (0, 0))
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.terminate()
+            pygame.display.flip()
+            self.clock.tick(self.fps)
+
+    def start_screen(self):
+        intro_text = ["ЗАСТАВКА", "",
+                      "Правила игры"]
+        fon = pygame.transform.scale(self.load_image('screen.jpg'), (self.width, self.height))
+        self.screen.blit(fon, (0, 0))
+        font = pygame.font.Font(None, 30)
+        text_coord = 50
+        for line in intro_text:
+            string_rendered = font.render(line, 1, pygame.Color('white'))
+            intro_rect = string_rendered.get_rect()
+            text_coord += 10
+            intro_rect.top = text_coord
+            intro_rect.x = 10
+            text_coord += intro_rect.height
+            self.screen.blit(string_rendered, intro_rect)
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.terminate()
+                elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                    return  # начинаем игру
+            pygame.display.flip()
+            self.clock.tick(self.fps)
+
+
+if __name__ == '__main__':
+    app = App()
+    app.start_screen()
+    app.run_game()
