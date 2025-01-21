e552848e851d2d3d9a14d5a09c448efbe9fcfaac
Оля <olga1809dok@rambler.ru>
2025-01-21 20:36:59 +0300

    Programm from Oli

--- programm.py
+++ programm.py
@@ -1,8 +1,8 @@
-
 import os
 import sys
+from pygame.draw import rect
+import random
 import pygame
-#C:\Users\Оля\PycharmProjects\git_project1\pythonProject1
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, app, tile_type, pos_x, pos_y):
@@ -29,6 +29,8 @@ class Hero(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect().move(
             app.tile_width * pos[0] + 15, app.tile_height * pos[1] + 5)
+        self.tail = []
+        self.alpha_surf = pygame.Surface((700, 400), pygame.SRCALPHA)
 
     def update(self, pos):
         self.rect.x += pos[0]
@@ -36,6 +38,17 @@ class Hero(pygame.sprite.Sprite):
         if pygame.sprite.spritecollideany(self, self.app.tiles_group):
             self.rect.x -= pos[0]
             self.rect.y -= pos[1]
+        self.tail.append([[self.rect.x - 5, self.rect.y - 8],
+                               [random.randint(0, 25) / 10 - 1, random.choice([0, 0])],
+                               random.randint(5, 8)])
+        for t in self.tail:
+            t[0][0] += t[1][0]
+            t[0][1] += t[1][1]
+            t[2] -= 0.5
+            t[1][0] -= 0.4
+            rect(self.alpha_surf, (255, 255, 255),([int(t[0][0]), int(t[0][1])], [int(t[2]) for i in range(2)]))
+            if t[2] <= 0:
+                self.tail.remove(t)
 
 
 class App:
@@ -51,6 +64,8 @@ class App:
         self.tiles_group = pygame.sprite.Group()
         self.player_group = pygame.sprite.Group()
         self.fps = 50
+        self.alpha_surf = pygame.Surface(self.screen.get_size(), pygame.SRCALPHA)
+
     def terminate(self):
         pygame.quit()
         sys.exit()
@@ -76,7 +91,7 @@ class App:
             for x in range(len(level[y])):
                 if level[y][x] == '.':
                     Tile(self, 'empty', x, y)
-                elif level[y][x] == '#':
+                if level[y][x] == '#':
                     self.tiles_group.add(Tile(self, 'wall', x, y))
                 elif level[y][x] == '@':
                     Tile(self, 'empty', x, y)
