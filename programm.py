674f0a20760ae2984000730e8c0475ee2907a6c0
Оля <olga1809dok@rambler.ru>
2025-01-26 11:21:40 +0300

    доделала стартовое окно

--- programm.py
+++ programm.py
@@ -131,13 +131,14 @@ class App:
     def start_screen(self):
         intro_text = [" ГеометрияДэш", "",
                       "Выбери уровень"]
-        self.screen.fill((0, 0, 0))
+        fon = pygame.transform.scale(self.load_image('screen.jpg'), (self.width, self.height))
+        self.screen.blit(fon, (0, 0))
         self.click1 = True
         self.click2 = False
         font = pygame.font.Font(None, 30)
         text_coord = 50
         for line in intro_text:
-            string_rendered = font.render(line, 0, pygame.Color('white'))
+            string_rendered = font.render(line, 0, pygame.Color('black'))
             intro_rect = string_rendered.get_rect()
             text_coord += 10
             intro_rect.top = text_coord
